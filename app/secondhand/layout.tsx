e612e5905d5f4d6ea49fa368d29ee143011ccc60
alititi-jackie <fengjiancheng8@gmail.com>
2026-05-14 21:15:36 -0400

    SEO: add /secondhand metadata in layout (page is client component)

---
+++ app/secondhand/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+
+const SECONDHAND_CANONICAL = 'https://app.openaa.com/secondhand'
+
+export const metadata: Metadata = {
+  title: 'OpenAA 二手｜美国华人二手市场｜纽约二手｜闲置转让',
+  description:
+    'OpenAA 二手频道为美国华人提供二手闲置和转让信息，涵盖纽约二手、家具、电器、手机、汽车用品、生活用品、搬家甩卖等内容，帮助华人更方便发布和查找二手市场信息。',
+  keywords: [
+    'OpenAA 二手',
+    '美国华人二手',
+    '纽约二手',
+    '华人二手市场',
+    '二手市场',
+    '闲置转让',
+    '搬家甩卖',
+    '二手家具',
+    '二手电器',
+    '二手手机',
+    '美国二手',
+    '纽约华人二手',
+    '美华人二手',
+  ],
+  alternates: {
+    canonical: SECONDHAND_CANONICAL,
+  },
+}
+
+export default function SecondhandLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
